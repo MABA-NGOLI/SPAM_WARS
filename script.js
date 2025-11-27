de541692b6430c766dec141878f0ac19dd752ec9
merveille <mabagloire3@gmail.com>
2025-11-27 16:20:23 +0100

    edit special cards

--- script.js
+++ script.js
@@ -33,3 +33,36 @@ function createFallingItem() {
 }
 
 setInterval(createFallingItem, 1200);
+
+
+document.querySelectorAll('.spec .toggle-icon').forEach(icon => {
+  icon.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    const article = icon.closest('.spec');
+    const title = article.querySelector('.card-title');
+    const p = article.querySelector('p');
+
+    // Vérifier si l'image existe déjà
+    let img = article.querySelector('.card-image');
+    if (!img) {
+      img = document.createElement('img');
+      img.src = article.getAttribute('data-image');
+      img.classList.add('card-image');
+      article.appendChild(img);
+    }
+
+    // Toggle affichage
+    const isVisible = img.style.display === 'block';
+    if (!isVisible) {
+      title.style.display = 'none';
+      p.style.display = 'none';
+      img.style.display = 'block';
+    } else {
+      title.style.display = 'inline'; // garde l'icône visible
+      p.style.display = 'block';
+      img.style.display = 'none';
+    }
+  });
+});
+
